8be39c6119e44c910ad99cd7b7aa8f7572ff553e
Francisco Ordóñez <fordonez@seroca.com>
2026-05-20 21:46:51 +0200

    Volver a Next.js 16, añadir .npmrc para legacy-peer-deps

--- auth.ts
+++ auth.ts
@@ -3,15 +3,13 @@ import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
-const tenantId = process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID!;
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     MicrosoftEntraID({
       clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID!,
       clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET!,
-      issuer: `https://login.microsoftonline.com/${tenantId}/v2.0`,
+      tenantId: process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID!,
     } as any),
   ],
   session: {
